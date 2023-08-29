de61d6966cf4dc36e9a6c30fc17e6a1ca7c404d4
JLighter <heroguelle.julien@gmail.com>
2023-08-29 18:24:14 +0200

    Remove nvimtree overrides from overrides.lua

--- .config/nvim/lua/custom/configs/overrides.lua
+++ .config/nvim/lua/custom/configs/overrides.lua
@@ -1,47 +1,33 @@
 local M = {}
 
 M.treesitter = {
-  ensure_installed = {
-    "vim",
-    "lua",
-    "html",
-    "css",
-    "javascript",
-    "typescript",
-    "tsx",
-    "c",
-    "markdown",
-    "markdown_inline",
-    "terraform",
-    "hcl",
-    "go",
-    "dockerfile",
-    "yaml"
-  },
-  indent = {
-    enable = true,
-    -- disable = {
-    --   "python"
-    -- },
-  },
-}
-
-M.nvimtree = {
-  open_on_setup = true,
-  ignore_ft_on_setup = { "dashboard" },
-  auto_close = true,
-  open_on_tab = false,
-  hijack_cursor = true,
-  update_cwd = true,
-  lsp_diagnostics = true,
-  update_focused_file = {
-    enable = true,
-    update_cwd = true,
-  },
-  system_open = {
-    cmd = nil,
-    args = {},
-  },
+	ensure_installed = {
+		"vim",
+		"lua",
+		"html",
+		"css",
+		"javascript",
+		"typescript",
+		"tsx",
+		"c",
+		"markdown",
+		"markdown_inline",
+		"terraform",
+		"hcl",
+		"go",
+		"dockerfile",
+		"yaml",
+	},
+	indent = {
+		enable = true,
+		-- disable = {
+		--   "python"
+		-- },
+	},
+	highlight = {
+		enable = true,
+		additional_vim_regex_highlighting = false,
+	},
 }
 
 return M
