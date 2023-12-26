423bf338ff2c29e36fd94a62920764639431f768
JLighter <heroguelle.julien@gmail.com>
2023-12-26 11:45:50 +0100

    Fix vspli of neogit

--- .config/nvim/lua/custom/plugins/neogit.lua
+++ .config/nvim/lua/custom/plugins/neogit.lua
@@ -38,7 +38,7 @@ return {
       ignored_settings = {},
       -- Change the default way of opening the commit popup
       commit_popup = {
-        kind = "split",
+        kind = "vsplit",
       },
       -- Change the default way of opening the preview buffer
       preview_buffer = {
@@ -56,18 +56,6 @@ return {
         hunk = { "", "" },
       },
       integrations = {
-        -- Neogit only provides inline diffs. If you want a more traditional way to look at diffs, you can use `sindrets/diffview.nvim`.
-        -- The diffview integration enables the diff popup, which is a wrapper around `sindrets/diffview.nvim`.
-        --
-        -- Requires you to have `sindrets/diffview.nvim` installed.
-        -- use {
-        --   'TimUntersberger/neogit',
-        --   requires = {
-        --     'nvim-lua/plenary.nvim',
-        --     'sindrets/diffview.nvim'
-        --   }
-        -- }
-        --
         diffview = true
       },
       -- Setting any section to `false` will make the section not render at all
